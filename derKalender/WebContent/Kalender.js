7502eae61feff31536e23d8e6a7f56dc2e0eb25d
Annika lap <Annika lap@LAPTOP-O68JE29A.fritz.box>
2018-11-01 19:54:18 +0100

    monatsKalender kalenderwoche eingefügt, noch nicht über mehere Spalten

--- derKalender/WebContent/Kalender.js
+++ derKalender/WebContent/Kalender.js
@@ -3,5 +3,7 @@ var m = t.getMonth()+1;
 var j = t.getYear() +1900;
 
 function Monat_wechsel(vor){}
-function Kalender(){}
 function MonatsKalender(Monat, Jahr) {}
+
+function WochenKalender(Tag, Monat, Jahr) {}
+function Wochen_wechsel(vor){}
