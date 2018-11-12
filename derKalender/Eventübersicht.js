a5682071190e5c7d6f94a7ad1d9efc059144cc0e
fabia <fabia@192.168.0.25>
2018-11-12 12:18:03 +0100

    Kleine Versuche die Datenbank einzurichten

---
+++ derKalender/Eventübersicht.js
@@ -0,0 +1,17 @@
+var request = indexDB.open('Accountdaten',1);
+
+request.onupgradeneeded=function() {
+	console.log('Datenbank angelegt');
+	
+	var db=this.result;
+	if(!db.objectStoreNames.contains('features')) {
+		store=db.createObjectStore('feautures', {
+			keypath:'key',
+			autoIncrement:true });
+		
+			1
+		}
+	}
+}/**
+ * 
+ */
