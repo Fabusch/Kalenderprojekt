351745fb433a1fc71b8e4251091dfcaccf0e73d1
Annika lap <Annika lap@LAPTOP-O68JE29A.fritz.box>
2018-11-18 13:17:27 +0100

    JahresKalender

--- derKalender/WebContent/Eventübersicht.js
+++ derKalender/WebContent/Eventübersicht.js
@@ -1,11 +1,11 @@
-var request = indexDB.open('Accountdaten',1);
+var request = indexDB.open('Accountdaten',1);		//DatenBamk = request
 
 request.onupgradeneeded=function() {
 	console.log('Datenbank angelegt');
 	
 	var db=this.result;
-	if(!db.objectStoreNames.contains('features')) {
-		store=db.createObjectStore('feautures', {
+	if(!db.objectStoreNames.contains('features')) {		
+		store=db.createObjectStore('feautures', {		//Tabelle = features
 			keypath:'key',
 			autoIncrement: true
 		});
