f14981e1012cf1eac23781c873e6c3e63f317915
fabia <fabia@192.168.0.25>
2018-11-19 10:51:55 +0100

    Weiteres bearbeiten der Datenbank

--- derKalender/WebContent/Eventübersicht.js
+++ derKalender/WebContent/Eventübersicht.js
@@ -33,3 +33,39 @@ request.onerror = function(event) {
 		  // Dies dient zur Behandlung von allen Fehlern in der Datenbank!
 		  alert("Fehler in der Datenbank behandeln: " + event.target.errorCode);
 		};
+		
+		const customerData = [
+			  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+			  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+			];
+		
+		const dbName = "the_name";
+
+		var request = indexedDB.open(dbName, 2);
+
+		request.onerror = function(event) {
+		  
+		};
+		request.onupgradeneeded = function(event) {
+		  var db = event.target.result;
+
+		  
+		  var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+		 
+		  objectStore.createIndex("name", "name", { unique: false }); // Index Erstellen damit nicht 2x der gleiche Name vorhanden sein darf
+		  objectStore.createIndex("email", "email", { unique: true }); // Um Nutzer anhand der Email zu identifizieren
+
+		  objectStore.transaction.oncomplete = function(event) { // Dient dazu den ObjektStore erst fertig erstellen zu lassen bevor Daten aufgespielt werden
+		    
+		    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+		    customerData.forEach(function(customer) {
+		      customerObjectStore.add(customer);
+		    });
+		  };
+		};
+		
+		
+		
+		
+		
