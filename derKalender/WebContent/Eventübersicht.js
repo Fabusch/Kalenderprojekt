3718faf778817c74eb47bc0703eef3fbe0a6fc00
Annika lap <Annika lap@LAPTOP-O68JE29A.fritz.box>
2018-11-20 14:18:00 +0100

    eingerückt

--- derKalender/WebContent/Eventübersicht.js
+++ derKalender/WebContent/Eventübersicht.js
@@ -14,197 +14,136 @@ request.onupgradeneeded=function() {
 
 
 request.onerror = function(event) {
-	};
-	request.onsuccess = function(event) {
-	};
+};
+request.onsuccess = function(event) {
+};
 	
-	var db;
-	var request = indexedDB.open("Accountdaten");
-	request.onerror = function(event) {
-	  alert("Ihr Browser muss die Datenbank Index unterstützen um die Applikation nutzen zu können");
-	};
-	request.onsuccess = function(event) {
-	  db = event.target.result;
-	};
+var db;
+var request = indexedDB.open("Accountdaten");
+request.onerror = function(event) {
+	alert("Ihr Browser muss die Datenbank Index unterstützen um die Applikation nutzen zu können");
+};
+request.onsuccess = function(event) {
+	db = event.target.result;
+};
 	
-	db.onerror = function(event) {
-		  // Dies dient zur Behandlung von allen Fehlern in der Datenbank!
-		  alert("Fehler in der Datenbank behandeln: " + event.target.errorCode);
-		};
+db.onerror = function(event) {
+	// Dies dient zur Behandlung von allen Fehlern in der Datenbank!
+	alert("Fehler in der Datenbank behandeln: " + event.target.errorCode);
+};
 		
-		const Nutzerdaten = [
-			  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-			  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-			];
-		
-		const dbName = "Accountdaten";
+const Nutzerdaten = [
+		{ ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+		{ ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+	];
+	
+const dbName = "Accountdaten";
 
-		var request = indexedDB.open(dbName, 2);
+var request = indexedDB.open(dbName, 2);
 
-		request.onerror = function(event) {
+request.onerror = function(event) {
 		  
-		};
-		request.onupgradeneeded = function(event) {
-		  var db = event.target.result;
+};
+request.onupgradeneeded = function(event) {
+	var db = event.target.result;
+  
+	var objectStore = db.createObjectStore("Accountdaten", { keyPath: "ssn" }); // Der Keypath macht das Objekt (die Person) einzigartig damit ist sie sofort zu identifizieren
+	// in meinem Beispiel ist dies die SSN (Sozialversicherungsnummer)
+	
+	objectStore.createIndex("name", "name", { unique: false }); // Index Erstellen damit nicht 2x der gleiche Name vorhanden sein darf
+	objectStore.createIndex("email", "email", { unique: true }); // Um Nutzer anhand der Email zu identifizieren
 
-		  
-		  var objectStore = db.createObjectStore("Accountdaten", { keyPath: "ssn" }); // Der
-																						// Keypath
-																						// macht
-																						// das
-																						// Objekt
-																						// (die
-																						// Person)
-																						// einzigartig
-																						// damit
-																						// ist
-																						// sie
-																						// sofort
-																						// zu
-																						// identifizieren
-		  // in meinem Beispiel ist dies die SSN (Sozialversicherungsnummer)
-
-		 
-		  objectStore.createIndex("name", "name", { unique: false }); // Index
-																		// Erstellen
-																		// damit
-																		// nicht
-																		// 2x
-																		// der
-																		// gleiche
-																		// Name
-																		// vorhanden
-																		// sein
-																		// darf
-		  objectStore.createIndex("email", "email", { unique: true }); // Um
-																		// Nutzer
-																		// anhand
-																		// der
-																		// Email
-																		// zu
-																		// identifizieren
-
-		  objectStore.transaction.oncomplete = function(event) { // Dient
-																	// dazu den
-																	// ObjektStore
-																	// erst
-																	// fertig
-																	// erstellen
-																	// zu lassen
-																	// bevor
-																	// Daten
-																	// aufgespielt
-																	// werden
+	objectStore.transaction.oncomplete = function(event) { // Dient dazu den ObjektStore erst fertig erstellen zu lassen bevor Daten aufgespielt werden
 		    
-		    var customerObjectStore = db.transaction("Accountdaten", "readwrite").objectStore("Accountdaten");
-		    customerData.forEach(function(customer) {
-		      customerObjectStore.add(customer);
-		    });
-		  };
+		var customerObjectStore = db.transaction("Accountdaten", "readwrite").objectStore("Accountdaten");
+		customerData.forEach(function(customer) {
+			customerObjectStore.add(customer);
+		});
+	};
 		  
 		  
-		  openReq.onblocked = function(event) {
+	openReq.onblocked = function(event) {
+		alert("Please close all other tabs with this site open!");
+	};
 			  
-			  alert("Please close all other tabs with this site open!");
-			};
+	openReq.onupgradeneeded = function(event) {
+		db.createObjectStore(/* ... */);
+		useDatabase(db);
+	};
 			  
-			openReq.onupgradeneeded = function(event) {
-			  
-			  db.createObjectStore(/* ... */);
-			  useDatabase(db);
-			};
-			  
-			openReq.onsuccess = function(event) {
-			  var db = event.target.result;
-			  useDatabase(db);
-			  return;
-			};
+	openReq.onsuccess = function(event) {
+		var db = event.target.result;
+		useDatabase(db);
+		return;
+	};
 
-			openReq.onblocked = function(event) {
-				
-				  alert("Schließen sie alle Seiten und aktualsieren sie die Geöffnete!");
-				};
+	openReq.onblocked = function(event) {
+		alert("Schließen sie alle Seiten und aktualsieren sie die Geöffnete!");
+	};
 				  
-				openReq.onupgradeneeded = function(event) {
-				  .
-				  db.createObjectStore(/* ... */);
-				  useDatabase(db);
-				};
+	openReq.onupgradeneeded = function(event) {
+		db.createObjectStore(/* ... */);
+		useDatabase(db);
+	};
 				  
-				openReq.onsuccess = function(event) {
-				  var db = event.target.result;
-				  useDatabase(db);
-				  return;
-				};
+	openReq.onsuccess = function(event) {
+		var db = event.target.result;
+		useDatabase(db);
+		return;
+	};
 
-				openReq.onblocked = function(event) {		
-					  alert("Schließen sie alle Tabs damit diese Seite richtig angezeigt werden kann");
-					};
+	openReq.onblocked = function(event) {		
+		alert("Schließen sie alle Tabs damit diese Seite richtig angezeigt werden kann");
+	};
 					  
-					openReq.onupgradeneeded = function(event) {
-					  db.createObjectStore(/* ... */);
-					  useDatabase(db);
-					};
+	openReq.onupgradeneeded = function(event) {
+		db.createObjectStore(/* ... */);
+		useDatabase(db);
+	};
 					  
-					openReq.onsuccess = function(event) {
-					  var db = event.target.result;
-					  useDatabase(db);
-					  return;
-					};
+	openReq.onsuccess = function(event) {
+		var db = event.target.result;
+		useDatabase(db);
+		return;
+	};
 					
-					function useDatabase(db) {
-						  db.onversionchange = function(event) {
-						    db.close();
-						    alert("Die Datenbank wurde aktualisiert bitte schließen sie das Fenster um die Änderungen wirksam zu machen");
-						  }};	
-						  
+	function useDatabase(db) {
+		db.onversionchange = function(event) {
+			db.close();
+			alert("Die Datenbank wurde aktualisiert bitte schließen sie das Fenster um die Änderungen wirksam zu machen");
+		}
+	};	
+};					  
 						  
 		// In der Datenbank Daten speichern
 		
-		var transaction = db.transaction(["Accountdaten"]), "readwrite)";  // Da
-																			// eine
-																			// Transaktion
-																			// jetzt
-																			// vorhanden
-																			// ist
-																			// müssen
-																			// wir
-																			// den
-																			// Objektspeicher
-																			// darauf
-																			// auffrufen
-		transaction.oncomplete = function(event) {
-			  alert("Alles erledigt");
-			};
+var transaction = db.transaction((["Accountdaten"]), "readwrite");  // Da eine Transaktion jetzt vorhanden ist müssen wir den Objektspeicher darauf auffrufen
+transaction.oncomplete = function(event) {
+	alert("Alles erledigt");
+};
 			
-			transaction.onerror = function(event) { // Errorbehandlung
-				};
+transaction.onerror = function(event) { // Errorbehandlung
+};
 		
-				var objectStore = transaction.objectStore("Accountdaten");
-				customerData.forEach(function(customer) {
-				  var request = objectStore.add(customer); // add. wenn sich
-															// noch kein Objekt
-															// mit demselben
-															// Schlüssen in der
-															// DB befindet falls
-															// doch put()
-															// verwenden.
-				  request.onsuccess = function(event) {
-				    // Ergebnis die Kunden SSN
-				  };
-				});
+var objectStore = transaction.objectStore("Accountdaten");
+customerData.forEach(function(customer) {
+	var request = objectStore.add(customer); // add. wenn sic noch kein Objekt mit demselben Schlüssen in der DB befindet falls doch put() verwenden.
+	request.onsuccess = function(event) {
+		// Ergebnis die Kunden SSN
+	};
+});
 				
 				
+		// In der Datenbank Daten löschen
 				
-				// In der Datenbank Daten löschen
-				
-				var request = db.transaction(["Accountdaten"], "readwrite")
-                .objectStore("Accountdaten")
-                .delete("444-44-4444");
-request.onsuccess = function(event)
+var request = db.transaction(["Accountdaten"], "readwrite")
+      .objectStore("Accountdaten")
+      .delete("444-44-4444");
+request.onsuccess = function(event){};
 
 
-// Daten aus der Datenbank aufrufen
+
+		// Daten aus der Datenbank aufrufen
 
 var transaction = db.transaction(["Accountdaten"]);
 var objectStore = transaction.objectStore("Accountdaten");
@@ -213,8 +152,7 @@ request.onerror = function(event) {
   
 };
 request.onsuccess = function(event) {
-  
-  alert("Name for SSN 444-44-4444 is " + request.result.name);
+	alert("Name for SSN 444-44-4444 is " + request.result.name);
 };
 
 
@@ -235,17 +173,14 @@ request.onerror = function(event) {
 };
 request.onsuccess = function(event) {
   
-  var data = event.target.result;
+	var data = event.target.result;
   
-  
-  data.age = 58;
+	data.age = 58;
 
-  
-  var requestUpdate = objectStore.put(data);
-   requestUpdate.onerror = function(event) {
-    
-   };
-   requestUpdate.onsuccess = function(event) {
-     
-   };
+	var requestUpdate = objectStore.put(data);
+	requestUpdate.onerror = function(event) {
+	};
+	requestUpdate.onsuccess = function(event) {
+	};
 };
+
