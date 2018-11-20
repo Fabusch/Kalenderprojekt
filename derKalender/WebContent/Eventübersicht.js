347a8d58bb0a4ccf1f387867766f3d7c623ff143
fabia <fabia@192.168.0.25>
2018-11-20 12:57:21 +0100

    Edit

--- derKalender/WebContent/Eventübersicht.js
+++ derKalender/WebContent/Eventübersicht.js
@@ -32,11 +32,6 @@ request.onerror = function(event) {
 		  alert("Fehler in der Datenbank behandeln: " + event.target.errorCode);
 		};
 		
-		
-		
-		
-		----------------------------------------------------------------------------------
-		
 		const Nutzerdaten = [
 			  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
 			  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
