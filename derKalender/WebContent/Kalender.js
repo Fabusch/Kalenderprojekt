de8fcd3ec8241bb8964b79ed3a81416ae5021233
Annika lap <Annika lap@LAPTOP-O68JE29A.fritz.box>
2018-11-30 14:35:00 +0100

    add/chage bilder

--- derKalender/WebContent/Kalender.js
+++ derKalender/WebContent/Kalender.js
@@ -35,7 +35,7 @@ request.onupgradeneeded = function(event){
 	for (var g in Gruppen){
 		ObjectStore.add(Gruppen[g]);
 	}
-	alert("Die Datenbank wurde angelegt");
+	//alert("Die Datenbank wurde angelegt");
 }
 
 
@@ -74,10 +74,10 @@ function einfügen(store, Werte){
 			.add(Werte);
 		
 		request.onsuccess = function(event) {
-			alert("Der "+store+" Datensatz wurde hinzugefügt.");
+			//alert("Der "+store+" Datensatz wurde hinzugefügt.");
 		};
 		request.onerror = function(event) {
-			alert("Der "+store+" Datensatz wurde NICHT hinzugefügt.!!!!!!");
+			//alert("Der "+store+" Datensatz wurde NICHT hinzugefügt.!!!!!!");
 		}
 	}
 }
@@ -159,10 +159,40 @@ Wochentag = new Array("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag",
 Gruppenmitglieder =new Array("Person 1","Person 2","Person 3"/*,"Person 4","Person 5","Person 6","Person 7"*/);
 			
 function background(element){
-	if(element.innerHTML=="Neptune")
-		document.getElementById('kalender').style.backgroundImage = "url('img/Neptune.png')";
-	else if(element.innerHTML=="Star-Wars Clone")
-		document.getElementById('kalender').style.backgroundImage = "url('img/Star-Wars_Clone.jpg')";
+	if(element.innerHTML=="1")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131014_133001.jpg')";
+	else if(element.innerHTML=="2")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131015_104048.jpg')";
+	else if(element.innerHTML=="3")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131018_121613.jpg')";
+	else if(element.innerHTML=="4")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131018_123317.jpg')";
+	else if(element.innerHTML=="5")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131019_054735.jpg')";
+	else if(element.innerHTML=="6")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131019_061059.jpg')";
+	else if(element.innerHTML=="7")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/10_Tansania_20131025_180933.jpg')";
+	else if(element.innerHTML=="8")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150130_110901.jpg')";
+	else if(element.innerHTML=="9")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150130_111014.jpg')";
+	else if(element.innerHTML=="10")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150408_112423.jpg')";
+	else if(element.innerHTML=="11")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150424_193728.jpg')";
+	else if(element.innerHTML=="12")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150424_194602.jpg')";
+	else if(element.innerHTML=="13")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150524_110726_Richtone(HDR).jpg')";
+	else if(element.innerHTML=="14")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150524_112047_Richtone(HDR).jpg')";
+	else if(element.innerHTML=="15")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150524_113024_Richtone(HDR).jpg')";
+	else if(element.innerHTML=="16")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150615_120601.jpg')";
+	else if(element.innerHTML=="17")
+		document.getElementById('kalender').style.backgroundImage = "url('img/Natur/20150722_120621.jpg')";
 }
 function Monatsende(aDate){
 	var Monat = aDate.getMonth() + 1;
