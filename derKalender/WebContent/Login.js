f463c15c77056df5ee27e1755485490f4bb37665
Annika lap <Annika lap@192.168.118.193>
2018-12-17 10:23:41 +0100

    eltern berichtigt

--- derKalender/WebContent/Login.js
+++ derKalender/WebContent/Login.js
@@ -162,8 +162,8 @@ function eltern(){
 					user.Kinder = user.Kinder.push(document.getElementById("nickname").value);
 					
 					updateRequest = objectStore.put(user);
-					updateTitleRequest.onsuccess = function() {
-						alert("das Kind "+document.getElementById("name").value+" wurfe hinzugefügt");
+					updateRequest.onsuccess = function() {
+						alert("Als Kind hinzugefügt");
 						window.location.href = "Profilübersicht.html";
 					};
 				}else{
@@ -174,38 +174,6 @@ function eltern(){
 		}
 	}
 }
-function dbAendern(store, Id, schluessel, Wert){	//schluessel bleib noch undefined
-	var request = window.indexedDB.open("Accountdaten",1);
-	request.onerror = function(event) {
-		console.log("error: ");
-		alert("Ihr Browser muss die Datenbank Index unterstützen um die Applikation nutzen zu können");
-	};
-
-	request.onsuccess = function(event){
-		db = request.result;
-		objectStore = db.transaction([store], "readwrite").objectStore(store);
-
-		// Get the to-do list object that has this title as it's title
-		objectStoreRequest = objectStore.get(Id);
-		
-		objectStoreRequest.onsuccess = function() {
-			// Grab the data object returned as the result
-			var data = objectStoreRequest.result;
-			// Update the notified value in the object to "yes"
-			data.schluessel = Wert;
-			// Create another request that inserts the item back into the database
-			var updateTitleRequest = objectStore.put(data);
-			
-			// Log the transaction that originated this request
-			console.log("The transaction that originated this request is " + updateTitleRequest.transaction);
-			// When this new request succeeds, run the displayData() function again to update the display
-			updateTitleRequest.onsuccess = function() {
-				displayData();
-			};
-		};
-	}
-}
-
 function date(){
 	fail  = validateVorname( document.getElementById("vorname").value )
 	fail += validateNachname( document.getElementById("nachname").value )
