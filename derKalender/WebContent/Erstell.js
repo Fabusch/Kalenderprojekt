016013842a845a0ae128b043e97d12ba0f9dfea0
Annika lap <Annika lap@LAPTOP-O68JE29A>
2019-01-12 15:45:36 +0100

    User nur einmal eintragbar

--- derKalender/WebContent/Erstell.js
+++ derKalender/WebContent/Erstell.js
@@ -80,68 +80,81 @@ function dbAendern(Id, Wert){
 
 function erstellGruppe(){
 	name= document.getElementById('name').value;
-	
-	Gruppe= []
-	Mitglieder= document.getElementById('Mitglieder').getElementsByTagName('a');
-	for(x=0; x <Mitglieder.length; x++){
-		Gruppe.push(Mitglieder[x].innerHTML);
-	}
-	
-	var request = window.indexedDB.open("Accountdaten",1);
-	request.onerror = function(event) {	
-		console.log("error: ");
-		alert("Ihr Browser muss die Datenbank Index unterstützen um die Applikation nutzen zu können");
-	};
-	request.onsuccess = function(event){
-		Db = request.result;	// Wenn die Datenbank vorhanden ist wird das hinzugefügt.
-		store = Db.transaction(["Gruppe"], "readwrite").objectStore("Gruppe")
-		request = store.add({name: name, Mitglieder: Gruppe});
+	if( !validateName(name) )
+		alert("Es wurde kein Name eingegeben.")
+	else if( !istGruppe())
+		alert("Alleine bist du keine Gruppe")
+	else{
+		Gruppe= []
+		Mitglieder= document.getElementById('Mitglieder').getElementsByTagName('a');
+		for(x=0; x <Mitglieder.length; x++){
+			Gruppe.push(Mitglieder[x].innerHTML);
+		}
 		
-		request.onsuccess = function(event) {
-			request = store.count();
-			request.onsuccess = function() {
-				id = request.result;
-				for(x=0; x <Gruppe.length; x++){
-					user= Gruppe[x]
-					dbAendern(user, id)
-				}
-				alert("Gruppe "+name+" wurde erstellt");
-			}
+		var request = window.indexedDB.open("Accountdaten",1);
+		request.onerror = function(event) {	
+			console.log("error: ");
+			alert("Ihr Browser muss die Datenbank Index unterstützen um die Applikation nutzen zu können");
 		};
-		request.onerror = function(event) {
-			alert("Der "+"Gruppe"+" Datensatz wurde NICHT hinzugefügt.");
+		request.onsuccess = function(event){
+			Db = request.result;	// Wenn die Datenbank vorhanden ist wird das hinzugefügt.
+			store = Db.transaction(["Gruppe"], "readwrite").objectStore("Gruppe")
+			request = store.add({name: name, Mitglieder: Gruppe});
+			
+			request.onsuccess = function(event) {
+				request = store.count();
+				request.onsuccess = function() {
+					id = request.result;
+					for(x=0; x <Gruppe.length; x++){
+						user= Gruppe[x]
+						dbAendern(user, id)
+					}
+					alert("Gruppe "+name+" wurde erstellt");
+				}
+			};
+			request.onerror = function(event) {
+				alert("Der "+"Gruppe"+" Datensatz wurde NICHT hinzugefügt.");
+			}
 		}
 	}
 }
 function erstellTermin(){
 	name= document.getElementById('terminname').value;
+	if(validateName(name)){	// der Termin muss einen Namen
+		Gruppe= []
+		Mitglieder= document.getElementById('Mitglieder').getElementsByTagName('a');	//Teilnehmer
+		for(x=0; x <Mitglieder.length; x++){
+			Gruppe.push(Mitglieder[x].innerHTML);
+		}
 	
-	Gruppe= []
-	Mitglieder= document.getElementById('Mitglieder').getElementsByTagName('a');	//Teilnehmer
-	for(x=0; x <Mitglieder.length; x++){
-		Gruppe.push(Mitglieder[x].innerHTML);
-	}
+		aDatum= new Date( document.getElementById('Datum').value);
+		aTime = ""+document.getElementById('uhrzeit').value
+		eDatum= new Date(document.getElementById('EndDatum').value);
+		eTime = ""+document.getElementById('Enduhrzeit').value
 	
-	aDatum= new Date( document.getElementById('Datum').value); // Zeitpunkt Start
-	aTime = ""+document.getElementById('uhrzeit').value
-	aHour = parseInt(aTime[0]+aTime[1])
-	aMin = parseInt(aTime[3]+aTime[4])
-	aDatum.setHours(aHour)
-	aDatum.setMinutes(aMin)
+		aHour = parseInt(aTime[0]+aTime[1])	 // Zeitpunkt Start
+		aMin = parseInt(aTime[3]+aTime[4])
+		aDatum.setHours(aHour)
+		aDatum.setMinutes(aMin)
 	
-	eDatum= new Date(document.getElementById('EndDatum').value);// Zeitpunkt Ende
-	eTime = ""+document.getElementById('Enduhrzeit').value
-	eHour = parseInt(eTime[0]+eTime[1])
-	eMin = parseInt(eTime[3]+eTime[4])
-	eDatum.setHours(eHour)
-	eDatum.setMinutes(eMin)
+		eHour = parseInt(eTime[0]+eTime[1])	// Zeitpunkt Ende
+		eMin = parseInt(eTime[3]+eTime[4])
+		eDatum.setHours(eHour)
+		eDatum.setMinutes(eMin)
 	
-	for(x in Gruppe){
-		user=Gruppe[x]
-		ID = einfügen("Termin",	{name: name, username: user, start: aDatum, ende: eDatum });
-//		alert("Termin erfolgreich erstellt")
-	}	
+		if( !(""+aDatum =="Invalid Date" && ""+eDatum =="Invalid Date")){
+			for(x in Gruppe){
+				user=Gruppe[x]
+				ID = einfügen("Termin",	{name: name, username: user, start: aDatum, ende: eDatum });
+//				alert("Termin erfolgreich erstellt")
+			}
+		}
+		else
+			alert ("Bitte trage Datum und Uhrzeit beim Start und End Zeitpunkt ein")
+	}else
+		alert ("Es wurde kein Name eingegeben.")
 }
+
 
 function logout(){
 	var request = window.indexedDB.open("Accountdaten",1);
@@ -165,7 +178,7 @@ function link(element){
 function linkout(element){
 	element.style.color= "black";
 }
-
+ 
 //nav
 function Gruppen(event) {
 	var x = document.getElementById('a' +event.id);
@@ -267,12 +280,11 @@ function Kalender(GID){
 	}
 }
 
-
 function Person(){
 	object = document.getElementById("Person");
 	name = object.value;
-	if(name=="") {
-		window.location.href = "Profilübersicht.html";}
+	
+	if(name=="") return;
 	else{
 		var request = window.indexedDB.open("Accountdaten",1);	//öffne indexedDB
 		request.onerror = function(event) {
@@ -289,17 +301,36 @@ function Person(){
 				if (request.result){
 					liste = document.getElementById("Mitglieder");
 					LinkGruppe = document.createElement("a");
-					LinkGruppe.innerHTML = name
-					liste.appendChild(LinkGruppe);
 					
-					br = document.createElement("br");
-					liste.appendChild(br);
+					User =[] 
+					Mitglieder= document.getElementById('Mitglieder').getElementsByTagName('a');
+					for(x=0; x <Mitglieder.length; x++){
+						User.push(Mitglieder[x].innerHTML);
+					}
+					if( User.indexOf(name) < 0){
+						LinkGruppe.innerHTML = name
+						liste.appendChild(LinkGruppe);
+						
+						br = document.createElement("br");
+						liste.appendChild(br);
+					}
 				}
 				else
 					alert(name +" ist nicht der Bernutzername einer unserer User")
 			}
 		}
 	}	
+}
+
+
+function validateName(field){
+	if (field =="") return false
+	return true
+}
+function istGruppe(){
+	if ( document.getElementById("Mitglieder").getElementsByTagName("a").length == 1)
+		return false 
+	return true
 }
 
 
